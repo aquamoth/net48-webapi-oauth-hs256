140591ed27a6a1cd77d164d5bbb4b70ed44ddff9
Mattias Åslund <mattias.aslund@shoppa.com>
2023-08-14 19:29:26 +0200

    Securing behind OAuth

---
+++ OauthWebClient/DebugLogger.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Owin.Logging;
+using System;
+using System.Diagnostics;
+
+namespace OauthWebClient
+{
+    public class LoggerFactory : ILoggerFactory
+    {
+        public ILogger Create(string name) => new DebugLogger();
+    }
+
+    public class DebugLogger : ILogger
+    {
+        public bool WriteCore(TraceEventType eventType, int eventId, object state, Exception exception, Func<object, Exception, string> formatter)
+        {
+            if (eventId == 0 && state == null) return true;
+
+            Debug.WriteLine($"{DateTime.UtcNow:HH:mm:ss.fff} [{eventId}] {formatter(state, exception)}");
+            return true;
+        }
+    }
+}
---
+++ OauthWebClient/Startup.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using Microsoft.Owin.Logging;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Jwt;
+using Owin;
+using System;
+using System.Configuration;
+using System.Text;
+
+namespace OauthWebClient
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.SetLoggerFactory(new LoggerFactory());
+
+            var securityKey = Convert.FromBase64String(ConfigurationManager.AppSettings["JwtKey"]);
+            string validIssuer = ConfigurationManager.AppSettings["JwtIssuer"];
+
+            var tokenValidationParameters = new TokenValidationParameters()
+            {
+
+                ValidateIssuer = true,
+                ValidateAudience = false,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = validIssuer,
+                ValidAudience = "",
+                IssuerSigningKey = new SymmetricSecurityKey(securityKey)
+            };
+
+            app.UseJwtBearerAuthentication(
+                new JwtBearerAuthenticationOptions
+                {
+                    AuthenticationMode = AuthenticationMode.Active,
+                    TokenValidationParameters = tokenValidationParameters
+                });
+        }
+    }
+}
